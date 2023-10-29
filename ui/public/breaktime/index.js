04820a1ba994b16bf2b2fb8fad83839078f815bd
19mdavenport <mdaven19@byu.edu>
2023-10-28 22:23:07 -0600

    canvas items

--- ui/public/breaktime/index.js
+++ ui/public/breaktime/index.js
@@ -73,3 +73,88 @@ function startBreak() {
     funcTimer();
     let timer = setInterval(funcTimer, 1000);
 }
+
+
+
+window.addEventListener("DOMContentLoaded", () => {
+
+
+    var myGamePiece;
+
+    function startGame() {
+        myGamePiece = new component(20, 20, "red", 10, 120);
+        myGameArea.start();
+    }
+
+    var myGameArea = {
+        canvas: document.querySelector("#game-canvas"),
+        isMouseHover: false,
+        start: function () {
+            this.canvas.width = this.canvas.offsetWidth;
+            this.canvas.height = this.canvas.offsetHeight;
+            //this.canvas.style.cursor = "none"; //hide the original cursor
+            this.context = this.canvas.getContext("2d");
+            this.interval = setInterval(updateGameArea, 20);
+
+            this.canvas.addEventListener("mouseleave", function (event) {
+                myGameArea.isMouseHover = false
+            }, false);
+            this.canvas.addEventListener("mouseover", function (event) {
+                myGameArea.isMouseHover = true
+            }, false);
+            window.addEventListener('mousemove', function (e) {
+                myGameArea.x = e.offsetX;
+                myGameArea.y = e.offsetY;
+            });
+        },
+        clear: function () {
+            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.setUpAnchors();
+        },
+        setUpAnchors: function () {
+            let spaceAvailable = Math.floor(Math.min(this.canvas.height, this.canvas.width)/5);
+            let size = Math.floor(spaceAvailable / 13);
+            let anchorOffsetX = (this.canvas.width - 6 * size * 10 - size) / 2;
+            let anchorOffsetY = (this.canvas.height - 6 * size * 10 - size) / 2;
+            for(let i = 0; i < 6; i++) {
+                for(let j = 0; j < 5; j++) {
+                    this.context.fillStyle = "white";
+                    this.context.fillRect(i * size * 12 + anchorOffsetX, j * size * 12 + size * 6 + anchorOffsetY, size, size);
+                }
+            }
+
+            for(let i = 0; i < 5; i++) {
+                for(let j = 0; j < 6; j++) {
+                    this.context.fillStyle = "white";
+                    this.context.fillRect(i * size * 12 + size * 6 + anchorOffsetX, j * size * 12 + anchorOffsetY, size, size);
+                }
+            }
+        }
+    }
+    function component(width, height, color, x, y) {
+        this.width = width;
+        this.height = height;
+        this.speedX = 0;
+        this.speedY = 0;
+        this.x = x;
+        this.y = y;
+        this.update = function () {
+            ctx = myGameArea.context;
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+
+    function updateGameArea() {
+        myGameArea.clear();
+        if (myGameArea.isMouseHover) {
+            myGamePiece.x = myGameArea.x;
+            myGamePiece.y = myGameArea.y;
+            myGamePiece.update();
+        }
+        
+    }
+
+    startGame();
+
+});
