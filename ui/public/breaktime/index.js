77426bdee47600a9f257974f6cdaa89b82657bf5
19mdavenport <mdaven19@byu.edu>
2023-10-28 23:03:56 -0600

    canvas anchors, guidelines

--- ui/public/breaktime/index.js
+++ ui/public/breaktime/index.js
@@ -89,6 +89,9 @@ window.addEventListener("DOMContentLoaded", () => {
     var myGameArea = {
         canvas: document.querySelector("#game-canvas"),
         isMouseHover: false,
+        size: 0,
+        anchorOffsetX: 0,
+        anchorOffsetY: 0,
         start: function () {
             this.canvas.width = this.canvas.offsetWidth;
             this.canvas.height = this.canvas.offsetHeight;
@@ -106,27 +109,27 @@ window.addEventListener("DOMContentLoaded", () => {
                 myGameArea.x = e.offsetX;
                 myGameArea.y = e.offsetY;
             });
+            this.setUpAnchors();
         },
         clear: function () {
             this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-            this.setUpAnchors();
         },
         setUpAnchors: function () {
-            let spaceAvailable = Math.floor(Math.min(this.canvas.height, this.canvas.width)/5);
-            let size = Math.floor(spaceAvailable / 13);
-            let anchorOffsetX = (this.canvas.width - 6 * size * 10 - size) / 2;
-            let anchorOffsetY = (this.canvas.height - 6 * size * 10 - size) / 2;
-            for(let i = 0; i < 6; i++) {
-                for(let j = 0; j < 5; j++) {
-                    this.context.fillStyle = "white";
-                    this.context.fillRect(i * size * 12 + anchorOffsetX, j * size * 12 + size * 6 + anchorOffsetY, size, size);
+            let spaceAvailable = Math.floor(Math.min(this.canvas.height, this.canvas.width) / 5);
+            this.size = Math.floor(spaceAvailable / 13);
+            this.anchorOffsetX = (this.canvas.width - 6 * this.size * 10 - this.size) / 2;
+            this.anchorOffsetY = (this.canvas.height - 6 * this.size * 10 - this.size) / 2;
+            this.context.fillStyle = "rgb(60, 60, 255)";
+            for (let i = 0; i < 6; i++) {
+                for (let j = 0; j < 5; j++) {
+                    this.context.fillRect(i * this.size * 12 + this.anchorOffsetX, j * this.size * 12 + this.size * 6 + this.anchorOffsetY, this.size, this.size);
                 }
             }
 
-            for(let i = 0; i < 5; i++) {
-                for(let j = 0; j < 6; j++) {
-                    this.context.fillStyle = "white";
-                    this.context.fillRect(i * size * 12 + size * 6 + anchorOffsetX, j * size * 12 + anchorOffsetY, size, size);
+            this.context.fillStyle = "rgb(255, 60, 60)";
+            for (let i = 0; i < 5; i++) {
+                for (let j = 0; j < 6; j++) {
+                    this.context.fillRect(i * this.size * 12 + this.size * 6 + this.anchorOffsetX, j * this.size * 12 + this.anchorOffsetY, this.size, this.size);
                 }
             }
         }
@@ -138,21 +141,44 @@ window.addEventListener("DOMContentLoaded", () => {
         this.speedY = 0;
         this.x = x;
         this.y = y;
-        this.update = function () {
+        this.draw = function () {
             ctx = myGameArea.context;
             ctx.fillStyle = color;
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
+        this.clear = function () {
+            ctx = myGameArea.context;
+            ctx.clearRect(this.x, this.y, this.width + 1, this.height + 1);
+        }
     }
 
     function updateGameArea() {
-        myGameArea.clear();
+        myGamePiece.clear();
         if (myGameArea.isMouseHover) {
-            myGamePiece.x = myGameArea.x;
-            myGamePiece.y = myGameArea.y;
-            myGamePiece.update();
+            let col = Math.floor((myGameArea.x - myGameArea.anchorOffsetX + myGameArea.size * 3) / (myGameArea.size * 6));
+            let row = Math.floor((myGameArea.y - myGameArea.anchorOffsetY + myGameArea.size * 3) / (myGameArea.size * 6));
+            if ((row + col) % 2 == 0) {
+                if(row % 2 == 1) {
+                    myGamePiece = new component(
+                        myGameArea.size * 11, 
+                        myGameArea.size, 
+                        "rgb(120, 120, 255)", 
+                        (col - 1) / 2 * myGameArea.size * 12 + myGameArea.anchorOffsetX + myGameArea.size, 
+                        (row - 1) / 2 * myGameArea.size * 12 + myGameArea.size * 6 + myGameArea.anchorOffsetY);
+                }
+                else {
+                    myGamePiece = new component(
+                        myGameArea.size, 
+                        myGameArea.size * 11, 
+                        "rgb(120, 120, 255)", 
+                        (col - 1) / 2 * myGameArea.size * 12 + myGameArea.size * 6 + myGameArea.anchorOffsetX, 
+                        (row - 1) / 2 * myGameArea.size * 12 + myGameArea.anchorOffsetY + myGameArea.size);
+                }
+                myGamePiece.draw();
+            }
+
         }
-        
+
     }
 
     startGame();
